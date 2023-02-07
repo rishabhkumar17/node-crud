81240d429d4dfd5b375e4f27a870f8b663f04967
sage <rishabhkaushal1796@gmail.com>
2023-02-07 22:22:09 +0530

    All events

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,14 @@
 // create a new express router
 const router = require('express').Router(),
 	mainController = require('./controllers/main.controller')
+	eventsController = require('./controllers/events.controller')
 	
 // export router
 module.exports = router
 
-// define router
+// define routes
+// main routes
 router.get('/', mainController.showHome)
+
+// event routes
+router.get('/events', eventsController.showEvents)
