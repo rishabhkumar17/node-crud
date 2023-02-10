e91b25249f6b46fdccde0b0dd70aa1570fb40f53
sage <rishabhkaushal1796@gmail.com>
2023-02-10 23:11:20 +0530

    show real events

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,44 +1,66 @@
 const Event = require('../models/event')
 
-module.exports = {
-	showEvents: showEvents,
-	showSingle: showSingle,
-	seedEvents: seedEvents
-}
-	// show all events
+// show all events
 const showEvents = (req, res) => {
-		//create dummy events
-		
-		//return a view with data
-		res.render('pages/events', {events: events})
-	};
+    // get all events
+    Event.find({}, (err, events) => {
+    	if(err) {
+    		res.status(404).send('Events not found!')
+    	}
 
-	// show a single event
+    	//return a view with data
+    	res.render('pages/events', { events: events })
+    })
+};
+
+// show a single event
 const showSingle = (req, res) => {
-		// get a single event
-		const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'}
-		
-		res.render('pages/single', { event: event})
-	};
+    // get a single event
+    Event.findOne({ slug: req.params.slug }, (err, event) => {
+    	if(err) {
+    		res.status(404).send('Event not found!')
+    	}
+    	res.render('pages/single', { event: event })
+    })
 
-	// seed our databasse 
+    
+};
+
+// seed our databasse 
 const seedEvents = (req, res) => {
-		// create some events 
-		const events = [
-			{ name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.'},
-			{ name: 'Swimming', slug: 'swimming', description: 'Michael Phelps is the fast fish.'},
-			{ name: 'Weightlifting', slug: 'weightlifting', description: 'Lifting heavy things up.'},
-			{name: 'Ping Pong', slug: 'pingpong', description: 'Super fast paddles'},
-		]
+    // create some events 
+    const events = [{
+            name: 'Basketball',
+            description: 'Throwing into a basket.'
+        },
+        {
+            name: 'Swimming',
+            description: 'Michael Phelps is the fast fish.'
+        },
+        {
+            name: 'Weightlifting',
+            description: 'Lifting heavy things up.'
+        },
+        {
+            name: 'Ping Pong',
+            description: 'Super fast paddles'
+        },
+    ]
 
-		// use the event model to insert/save
-		Event.remove({}, () => {
-			for(event of events) {
-			var newEvent = new Event(event)
-			newEvent.save()
-			}	
-		})
-		
-		// seeded
-		res.send('Database seeded!')
-	}
+    // use the event model to insert/save
+    Event.remove({}, () => {
+        for (event of events) {
+            var newEvent = new Event(event)
+            newEvent.save()
+        }
+    })
+
+    // seeded
+    res.send('Database seeded!')
+}
+
+module.exports = {
+    showEvents: showEvents,
+    showSingle: showSingle,
+    seedEvents: seedEvents
+}
